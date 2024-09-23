9ef643e6070cd01a844896a5dda88a58ef718993
AdityaKatiyar <katiyaraditya7236@gmail.com>
2024-09-23 17:31:56 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,8 @@
+// ====================== Typing animation=========================
+
+var typed  = new Typed(".typing",{
+    strings:["Web Designer", "Graphic Designer", "YouTuber","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
